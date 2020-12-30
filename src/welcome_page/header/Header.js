291ef3eef773ac4cce610451eea50e6d73582b44
bodroghykristof <bodroghykristof@gmail.com>
2020-12-30 23:42:46 +0100

    feat: display logo on header

--- src/welcome_page/header/Header.js
+++ src/welcome_page/header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import HeaderBar from './HeaderBar';
+import logo from './logo.png';
 
 function Header() {
-  return <HeaderBar></HeaderBar>;
+  return (
+    <HeaderBar>
+      <img src={logo} height='80%' alt='logo'></img>
+    </HeaderBar>
+  );
 }
 
 export default Header;
