1755795c960a75bfc021b1830b8dd1eeaee6901c
bodroghykristof <bodroghykristof@gmail.com>
2020-12-31 00:00:36 +0100

    feat: add WelcomeTextBox component

--- src/welcome_page/WelcomeCard.js
+++ src/welcome_page/WelcomeCard.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import WelcomeContainer from './WelcomeContainer';
+import WelcomeTextBox from './WelcomeTextBox';
 
 function WelcomeCard() {
-  return <WelcomeContainer></WelcomeContainer>;
+  return (
+    <WelcomeContainer>
+      <WelcomeTextBox>
+        <h2>Welcome to Friendly Fights!</h2>
+      </WelcomeTextBox>
+    </WelcomeContainer>
+  );
 }
 
 export default WelcomeCard;
