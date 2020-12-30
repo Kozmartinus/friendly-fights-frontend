a9701e6045aeb48520f7fde366c7ec5ebc03704a
bodroghykristof <bodroghykristof@gmail.com>
2020-12-30 22:31:19 +0100

    feat: add main route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return <p>Application content</p>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Route exact path='/'>
+        <p>Welcome Page</p>
+      </Route>
+    </Router>
+  );
+};
 
 export default App;
