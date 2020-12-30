209a241c373d60422b9ce3117907e2fa0804e242
bodroghykristof <bodroghykristof@gmail.com>
2020-12-30 23:13:43 +0100

    feat: display welcome card on its proper position

--- src/welcome_page/WelcomeCard.js
+++ src/welcome_page/WelcomeCard.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import WelcomeContainer from './WelcomeContainer';
 
 function WelcomeCard() {
-  return <div></div>;
+  return <WelcomeContainer></WelcomeContainer>;
 }
 
 export default WelcomeCard;
---
+++ src/welcome_page/WelcomeContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const HeaderBar = styled.div`
+  background-color: grey;
+  width: 60vw;
+  height: 60vh;
+  margin: 10vh auto;
+`;
+
+export default HeaderBar;
