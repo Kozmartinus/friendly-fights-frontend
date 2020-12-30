fefe1169815b353c3f1975f562605363314b6bdc
bodroghykristof <bodroghykristof@gmail.com>
2020-12-30 22:33:48 +0100

    feat: add Welcome component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Welcome from './welcome_page/Welcome';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/'>
-        <p>Welcome Page</p>
-      </Route>
+      <Route exact path='/' component={Welcome} />
     </Router>
   );
 };
