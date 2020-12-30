6f27aea9c79cab30e03b6811bec6ae6b91408b42
bodroghykristof <bodroghykristof@gmail.com>
2020-12-31 00:09:09 +0100

    feat: align text on WelcomeTextBox

--- src/welcome_page/WelcomeCard.js
+++ src/welcome_page/WelcomeCard.js
@@ -7,6 +7,10 @@ function WelcomeCard() {
     <WelcomeContainer>
       <WelcomeTextBox>
         <h2>Welcome to Friendly Fights!</h2>
+        <p>Time to challenge your friends in your selected games</p>
+        <p>Hang out with your gang in a competitive way</p>
+        <p>Organize your groups, record and follow your results</p>
+        <p>Ready to go? Who will be the champion today?</p>
       </WelcomeTextBox>
     </WelcomeContainer>
   );
--- src/welcome_page/WelcomeTextBox.js
+++ src/welcome_page/WelcomeTextBox.js
@@ -1,5 +1,15 @@
 import styled from 'styled-components';
 
-const WelcomeTextBox = styled.div``;
+const WelcomeTextBox = styled.div`
+  background-color: orange;
+  width: 60%;
+  height: 100%;
+  text-align: center;
+
+  h2 {
+    padding-top: 10%;
+    margin-bottom: 10%;
+  }
+`;
 
 export default WelcomeTextBox;
