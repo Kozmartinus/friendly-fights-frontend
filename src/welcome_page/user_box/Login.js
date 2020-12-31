552209c6764760994ebc4464da344cc02ae4ac39
bodroghykristof <bodroghykristof@gmail.com>
2020-12-31 11:22:10 +0100

    feat: add raw content of Login component

--- src/welcome_page/user_box/Login.js
+++ src/welcome_page/user_box/Login.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { GoogleLoginButton } from 'react-social-login-buttons';
+import { FacebookLoginButton } from 'react-social-login-buttons';
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div>
+      <input type='text'></input>
+      <br></br>
+      <input type='text'></input>
+      <br></br>
+      <p>Forgot my password</p>
+      <button>Login</button>
+      <GoogleLoginButton onClick={() => alert('Hello')} />
+      <FacebookLoginButton onClick={() => alert('Hello')} />
+    </div>
+  );
 }
 
 export default Login;
