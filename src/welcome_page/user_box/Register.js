0beb0b1d2947755fc14585182b7aabaad6456c28
bodroghykristof <bodroghykristof@gmail.com>
2020-12-31 11:52:08 +0100

    feat: add raw content to Register

--- src/welcome_page/user_box/Register.js
+++ src/welcome_page/user_box/Register.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import UserFormContainer from './UserFormContainer';
 
 function Register() {
-  return <div>Register</div>;
+  return (
+    <UserFormContainer>
+      <input type='text'></input>
+      <br></br>
+      <input type='text'></input>
+      <br></br>
+      <input type='text'></input>
+      <br></br>
+      <span>Upload profile picture</span>
+      <button>Browse</button>
+      <br></br>
+      <button id='loginConfirmationButton'>Register</button>
+    </UserFormContainer>
+  );
 }
 
 export default Register;
