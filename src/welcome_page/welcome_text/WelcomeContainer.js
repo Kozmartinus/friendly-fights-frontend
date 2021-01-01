35df5379053d2da5fa6de06cc5594bc5ff3adc71
Márton Kozma <kozmartinus@gmail.com>
2021-01-02 00:34:43 +0100

    refactor: remove comment

--- src/welcome_page/welcome_text/WelcomeContainer.js
+++ src/welcome_page/welcome_text/WelcomeContainer.js
@@ -4,7 +4,6 @@ const WelcomeContainer = styled.div`
   background-color: rgba(220, 220, 220, 0.9);
   width: 60vw;
   height: 60vh;
-  /* opacity: 0.9; */
   margin: 0 auto 12.5vh auto;
   padding-top: 12.5vh;
   background-clip: content-box;
