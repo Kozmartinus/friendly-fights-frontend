b526d0244b57e389862d5e91f2efd0a64091a282
bodroghykristof <bodroghykristof@gmail.com>
2021-01-02 13:51:14 +0100

    feat: implement form validation on login

--- src/welcome_page/user_box/Login.js
+++ src/welcome_page/user_box/Login.js
@@ -1,23 +1,40 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import UserFormContainer from './UserFormContainer';
 import { GoogleLoginButton } from 'react-social-login-buttons';
 import { FacebookLoginButton } from 'react-social-login-buttons';
 
-const forgotPasswordAction = () =>
-  alert('Shame on you, we do not have any smart solution for this yet');
-
 function Login() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const userName = useRef(null);
+  const password = useRef(null);
+
+  const forgotPasswordAction = () =>
+    alert('Shame on you, we do not have any smart solution for this yet');
+
+  const checkLogin = () => {
+    const userNameInput = userName.current.value;
+    const passwordInput = password.current.value;
+
+    if (userNameInput === '' || passwordInput === '') {
+      setErrorMessage('Please fill out all input fields!');
+    } else {
+      window.location.replace('/test');
+    }
+  };
+
   return (
     <UserFormContainer>
-      <input type='text' placeholder='Username'></input>
+      <input type='text' placeholder='Username' ref={userName}></input>
       <br></br>
-      <input type='password' placeholder='Password'></input>
+      <input type='password' placeholder='Password' ref={password}></input>
       <br></br>
-      <p>Invalid username or password!</p>
+      <p>{errorMessage}</p>
       <p id='forgotPassword' onClick={() => forgotPasswordAction()}>
         Forgot my password
       </p>
-      <button id='loginConfirmationButton'>Login</button>
+      <button id='loginConfirmationButton' onClick={checkLogin}>
+        Login
+      </button>
       <div className='socialMediaLoginContainer'>
         <GoogleLoginButton
           className='socialMediaLoginButton'
