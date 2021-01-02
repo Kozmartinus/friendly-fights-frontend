fd386ca300002c1ca54d9e1dac680e520ab097f8
bodroghykristof <bodroghykristof@gmail.com>
2021-01-02 13:40:26 +0100

    feat: implement client-side validation for registration

--- src/welcome_page/user_box/Register.js
+++ src/welcome_page/user_box/Register.js
@@ -1,20 +1,45 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import UserFormContainer from './UserFormContainer';
 
 function Register() {
+  const [errorMessage, setErrorMessage] = useState('');
+  const userName = useRef(null);
+  const passwordOne = useRef(null);
+  const passwordTwo = useRef(null);
+
+  const checkRegistration = () => {
+    const userNameInput = userName.current.value;
+    const passwordOneInput = passwordOne.current.value;
+    const passwordTwoInput = passwordTwo.current.value;
+
+    if (
+      userNameInput === '' ||
+      passwordOneInput === '' ||
+      passwordTwoInput === ''
+    ) {
+      setErrorMessage('Please fill out all input fields!');
+    } else if (passwordOneInput !== passwordTwoInput) {
+      setErrorMessage('Mismatching passwords!');
+    } else {
+      window.location.replace('/test');
+    }
+  };
+
   return (
     <UserFormContainer>
-      <input type='text' placeholder='Username'></input>
+      <input type='text' placeholder='Username' ref={userName}></input>
       <br></br>
-      <input type='text' placeholder='Password'></input>
+      <input type='text' placeholder='Password' ref={passwordOne}></input>
       <br></br>
-      <input type='text' placeholder='Password Again'></input>
+      <input type='text' placeholder='Password Again' ref={passwordTwo}></input>
       <br></br>
-      <p>This username is already taken!</p>
+      <p>{errorMessage}</p>
       <span>Upload profile picture</span>
       <button>Browse</button>
       <br></br>
-      <button id='loginConfirmationButton'>Register</button>
+      <button id='loginConfirmationButton' onClick={checkRegistration}>
+        Register
+      </button>
     </UserFormContainer>
   );
 }
