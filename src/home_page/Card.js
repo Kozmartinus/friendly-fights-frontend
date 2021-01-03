b98fc45de4fa43616a7eccc990c2d479fd5baaa0
Márton Kozma <kozmartinus@gmail.com>
2021-01-03 15:17:00 +0100

    style: add url for background image

--- src/home_page/Card.js
+++ src/home_page/Card.js
@@ -10,7 +10,8 @@ const Card = styled.div`
   margin: 6px 12px;
   position: relative;
   overflow: hidden;
-  background-image: url("${(props) => props.image}");
+  /* background-image: url("${(props) => props.image}"); */
+  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png");
   background-repeat: no-repeat;
   background-size: 100% 100%;
 `;
