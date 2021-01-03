93dbb49765db01576a42676f0bb7e310ca4b7f4d
bodroghykristof <bodroghykristof@gmail.com>
2021-01-03 17:30:15 +0100

    init: add empty ThemeProvider to component tree

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+
 import Welcome from './welcome_page/Welcome';
+import mainTheme from './general/styles/MainTheme';
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/' component={Welcome} />
+      <ThemeProvider theme={mainTheme}>
+        <Route exact path='/' component={Welcome} />
+      </ThemeProvider>
     </Router>
   );
 };
