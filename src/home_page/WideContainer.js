0a00de81db1539858bfd2f58c3dcb818fa0427b1
Márton Kozma <kozmartinus@gmail.com>
2021-01-03 14:07:13 +0100

    style: add initial css of WideContainer

---
+++ src/home_page/WideContainer.js
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+const WideContainer = styled.div`
+  background-color: rgba(220, 220, 220, 0.9);
+  width: 90vw;
+  height: 60vh;
+  margin: 0 auto 12.5vh auto;
+  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  background-clip: content-box;
+  display: flex;
+  flex-wrap: wrap;
+  border: solid white 3px;
+  border-radius: 20px;
+
+  /* @media only screen and (max-width: 650px) {
+    & {
+      width: 80vw;
+      height: 90vh;
+      margin: 0 auto 5vh auto;
+
+      display: flex;
+      flex-direction: column;
+    }
+  } */
+`;
+
+export default WideContainer;
