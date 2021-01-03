ab71849ea27cf0849ecf695debc9ed1ddac9819d
Márton Kozma <kozmartinus@gmail.com>
2021-01-03 11:58:37 +0100

    feat: add logged_in check for "/" route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Welcome from './welcome_page/Welcome';
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Welcome from "./welcome_page/Welcome";
+import Home from "./home_page/Home";
 
 const App = () => {
   return (
     <Router>
-      <Route exact path='/' component={Welcome} />
+      <Route
+        exact
+        path="/"
+        component={localStorage.getItem("user") === null ? Welcome : Home}
+      />
     </Router>
   );
 };
