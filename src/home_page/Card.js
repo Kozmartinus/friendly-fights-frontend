5c965a7be17a624de50ca2a0c993870cd6beb987
Márton Kozma <kozmartinus@gmail.com>
2021-01-03 17:02:29 +0100

    feat: add terniary for dotted/solid border

--- src/home_page/Card.js
+++ src/home_page/Card.js
@@ -4,14 +4,20 @@ const Card = styled.div`
   border-radius: 50px 10px 50px 10px;
   -moz-border-radius: 50px 10px 50px 10px;
   -webkit-border-radius: 50px 10px 50px 10px;
-  border: 2px solid #000000;
+  border: 2px
+    ${(props) => {
+      // console.log(props);
+
+      return props.plus === "true" ? "dotted" : "solid";
+    }}
+    #000000;
   width: 200px;
   height: 125px;
   margin: 6px 12px;
   position: relative;
   overflow: hidden;
-  /* background-image: url("${(props) => props.image}"); */
-  background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png");
+  background-image: url("${(props) => props.image}");
+  /* background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/1200px-Image_created_with_a_mobile_phone.png"); */
   background-repeat: no-repeat;
   background-size: 100% 100%;
 `;
