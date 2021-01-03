65059f33536690b4f8087c58e4d7d7b76846c9d6
Márton Kozma <kozmartinus@gmail.com>
2021-01-03 11:58:56 +0100

    feat: add initial Home page

---
+++ src/home_page/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "./../welcome_page/header/Header";
+
+const Home = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      you are logged in
+    </React.Fragment>
+  );
+};
+
+export default Home;
