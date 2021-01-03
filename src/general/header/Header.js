6e591354b2659f25678b104e35d7090ca4d75e6a
bodroghykristof <bodroghykristof@gmail.com>
2021-01-03 20:11:16 +0100

    feat: add images for home and profile icon

--- src/general/header/Header.js
+++ src/general/header/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import HeaderBar from './HeaderBar';
 import logo from './images/logo.png';
+import home from './images/home4.png';
+import profile from './images/profile.png';
 
 function Header() {
   return (
@@ -9,7 +11,10 @@ function Header() {
         <img src={logo} height='100%' alt='logo'></img>
       </div>
       <h1>Friendly Fights</h1>
-      <div className='imageContainer'></div>
+      <div className='imageContainer'>
+        <img src={home} height='100%' alt='home'></img>
+        <img src={profile} height='100%' alt='profile'></img>
+      </div>
     </HeaderBar>
   );
 }
